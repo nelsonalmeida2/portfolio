59d6ff6e50294899ee2c0287edf1f0f1b73c7633
Marmelson <152643202+Marmelson@users.noreply.github.com>
2025-02-19 19:14:24 +0000

    url updates

--- projetos.js
+++ projetos.js
@@ -4,21 +4,21 @@ const projetos = [
       "title": "Sea Battle",
       "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
       "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-      "github": "https://github.com/nelsonFAlmeida/nelsonFAlmeida.github.io"
+      "github": "https://github.com/nelsonalmeida2/SeaBattle"
     },
     {
         "id": 2,
         "title": "World Meter",
         "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
         "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-        "github": "https://github.com/nelsonFAlmeida/nelsonFAlmeida.github.io"
+        "github": "https://github.com/nelsonalmeida2/WorldMeter"
       },
     {
         "id": 3,
         "title": "The Night of the Living ",
         "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
         "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-        "github": "https://github.com/nelsonFAlmeida/nelsonFAlmeida.github.io"
+        "github": "https://github.com/nelsonalmeida2/The-Night-of-the-Living"
       },
       {
         "id": 4,
