9d2fab3b0cbabf55badb11255f4a4af7ba5397df
Marmelson <152643202+Marmelson@users.noreply.github.com>
2025-02-27 10:52:17 +0000

    projetos update

--- projetos.js
+++ projetos.js
@@ -1,42 +1,70 @@
 const projetos = [
     {
       "id": 1,
-      "title": "Sea Battle",
+      "title": "The Night of the Living",
       "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
       "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
       "github": "https://github.com/nelsonalmeida2/SeaBattle"
     },
     {
-        "id": 2,
-        "title": "World Meter",
+      "id": 2,
+      "title": "Web Interfaces",
+      "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+      "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      "github": "https://github.com/nelsonalmeida2/SeaBattle"
+    },
+    {
+        "id": 3,
+        "title": "Card Identifier System",
         "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
         "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
         "github": "https://github.com/nelsonalmeida2/WorldMeter"
       },
+      {
+        "id": 4,
+        "title": "Student Management Systems",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+        "github": "https://github.com/nelsonalmeida2/SeaBattle"
+      },
+      {
+        "id": 5,
+        "title": "Irrigation System",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+        "github": "https://github.com/nelsonalmeida2/WorldMeter"
+      },
+      {
+        "id": 6,
+        "title": "Database of a Documentary System",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+        "github": "https://github.com/nelsonalmeida2/WorldMeter"
+      },
+      {
+        "id": 7,
+        "title": "World Meter",
+        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+        "github": "https://github.com/nelsonFAlmeida/nelsonFAlmeida.github.io"
+      },
     {
-        "id": 3,
-        "title": "The Night of the Living ",
+        "id": 8,
+        "title": "Coffee Machine in assembly",
         "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
         "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
         "github": "https://github.com/nelsonalmeida2/The-Night-of-the-Living"
       },
       {
-        "id": 4,
-        "title": "The Night of the Living ",
+        "id": 9,
+        "title": "Mortal Kombat",
         "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
         "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
         "github": "https://github.com/nelsonFAlmeida/nelsonFAlmeida.github.io"
       },
       {
-        "id": 5,
-        "title": "The Night of the Living ",
-        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-        "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-        "github": "https://github.com/nelsonFAlmeida/nelsonFAlmeida.github.io"
-      },
-      {
-        "id": 6,
-        "title": "The Night of the Living ",
+        "id": 10,
+        "title": "Sea Battle",
         "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
         "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
         "github": "https://github.com/nelsonFAlmeida/nelsonFAlmeida.github.io"
